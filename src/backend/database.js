7878af916e48633d43f929c279d72acf7d1feef9
Shobana <mathiarulshobana@gmail.com>
2023-04-08 20:11:54 +0200

    Added search, sort and refactored styles.

--- src/backend/database.js
+++ src/backend/database.js
@@ -4,11 +4,11 @@ require("dotenv").config();
 const knex = require("knex")({
   client: "mysql2",
   connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLNAME,
 
   },
   pool: { min: 0, max: 7 },
